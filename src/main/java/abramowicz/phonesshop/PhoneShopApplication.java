e53bc7bd1fa7e1ce2854c9f093d3946944857ee0
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-06-24 08:21:58 +0200

    Main run app file added

---
+++ src/main/java/abramowicz/phonesshop/PhoneShopApplication.java
@@ -0,0 +1,15 @@
+package abramowicz.phonesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class PhoneShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneShopApplication.class, args);
+	}
+
+}
