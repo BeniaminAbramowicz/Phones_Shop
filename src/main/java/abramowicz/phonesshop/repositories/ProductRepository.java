8826647a98647afca4951f142215bc20bfaf60e4
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-07-22 11:48:16 +0200

    Added query to product repository for updating product in database

--- src/main/java/abramowicz/phonesshop/repositories/ProductRepository.java
+++ src/main/java/abramowicz/phonesshop/repositories/ProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -36,5 +37,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Modifying
     @Query(value = "DELETE FROM product WHERE product_id=:productId", nativeQuery = true)
     void deleteProduct(@Param("productId") int productId);
+
+    @Modifying
+    @Query(value = "UPDATE product p SET p.name=:name, p.description=:description, p.price=:price, p.items_number=:itemsNumber, p.picture=:picture, p.is_accessory=isAccessory WHERE p.product_id=:productId", nativeQuery = true)
+    void editProduct(@Param("name") String name, @Param("description") String description, @Param("price") BigDecimal price, @Param("itemsNumber") int itemsNumber, @Param("picture") String picture, @Param("isAccessory") Boolean isAccessory);
+
 
 }
