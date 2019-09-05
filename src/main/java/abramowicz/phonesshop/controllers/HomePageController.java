5cfc33e80b5a1bfa1bc9d4fdc5b5f859dcecebb1
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-09-05 16:45:45 +0200

    Modified homepage controller to get logged username

--- src/main/java/abramowicz/phonesshop/controllers/HomePageController.java
+++ src/main/java/abramowicz/phonesshop/controllers/HomePageController.java
@@ -1,13 +1,30 @@
 package abramowicz.phonesshop.controllers;
 
+import abramowicz.phonesshop.entities.User;
+import abramowicz.phonesshop.service.UserService;
+import abramowicz.phonesshop.utilities.UserUtilities;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
 
+    private final UserService userService;
+
+    @Autowired
+    public HomePageController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @GetMapping(value = {"/", "/index"})
-    public String displayHomePage(){
+    public String displayHomePage(Model model){
+        String username = UserUtilities.getLoggedUsername();
+        User user = userService.getUserByEmail(username);
+        model.addAttribute("user", user);
+
         return "index";
     }
 }
